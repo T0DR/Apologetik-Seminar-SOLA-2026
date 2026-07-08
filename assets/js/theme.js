3723f65305b0ac2c5e02b82a769ccd3cbe32cf19
T0DR <todor-subscriptions@protonmail.com>
2026-07-08 14:06:28 +0200

    Create theme.js

---
+++ assets/js/theme.js
@@ -0,0 +1,17 @@
+function toggleTheme() {
+    const body = document.body;
+
+    body.classList.toggle("dark-mode");
+
+    if (body.classList.contains("dark-mode")) {
+        localStorage.setItem("theme", "dark");
+    } else {
+        localStorage.setItem("theme", "light");
+    }
+}
+
+window.onload = function () {
+    if (localStorage.getItem("theme") === "dark") {
+        document.body.classList.add("dark-mode");
+    }
+};
